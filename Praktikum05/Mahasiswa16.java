92801f627f3e6bbcb8c5d8e85ec4c610b23757d1
nadznyu <nadiaalya0107@gmail.com>
2026-04-01 10:23:29 +0700

    Praktikum 2 - Bubble Sort (class Mahasiswa)

---
+++ Praktikum05/Mahasiswa16.java
@@ -0,0 +1,26 @@
+package Praktikum05;
+
+public class Mahasiswa16 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    Mahasiswa16() {
+
+    }
+
+    Mahasiswa16(String nm, String name, String kls, double ip) {
+        nim = nm;
+        nama = name;
+        ipk = ip;
+        kelas = kls;
+    }
+
+    void tampilInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK: " + ipk);
+    }
+}
