ccdfe8eb9f8a0a6fb71132295a9cfc836f8129f7
nadznyu <nadiaalya0107@gmail.com>
2026-04-01 11:25:55 +0700

    Modifikasi Insertion Sort (class Mapres / desc)

--- Praktikum05/MahasiswaBerprestasi16.java
+++ Praktikum05/MahasiswaBerprestasi16.java
@@ -49,7 +49,7 @@ public class MahasiswaBerprestasi16 {
         for (int i = 1; i < listMhs.length; i++) {
             Mahasiswa16 temp = listMhs[i];
             int j = i;
-            while (j > 0 && listMhs[j-1].ipk > temp.ipk) {
+            while (j > 0 && listMhs[j-1].ipk < temp.ipk) {
                 listMhs[j] = listMhs [j-1];
                 j--;
             }
