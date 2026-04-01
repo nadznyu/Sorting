b8026ffbe2693a2266c2475247cf786375c1cf7d
nadznyu <nadiaalya0107@gmail.com>
2026-04-01 09:58:55 +0700

    Praktikum 1 - Sorting Insertion Sort (class SortingMain)

--- Praktikum05/SortingMain16.java
+++ Praktikum05/SortingMain16.java
@@ -20,5 +20,16 @@ public class SortingMain16 {
         dataurut2.SelectionSort();
         System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
         dataurut2.tampil();
+        System.out.println();
+
+        int c[] = {40, 10, 4, 9, 3};
+        Sorting16 dataurut3 = new Sorting16(c, c.length);
+
+        System.out.println("Data awal 3");
+        dataurut3.tampil();
+        dataurut3.insertionSort();
+        System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+        dataurut3.tampil();
+
     }
 }
