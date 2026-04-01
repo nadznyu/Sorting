dd1396350e6e5f76f60751461f1ad5824cd4b236
nadznyu <nadiaalya0107@gmail.com>
2026-04-01 09:58:06 +0700

    Praktikum 1 - Sorting Insertion Sort (class Sorting)

--- Praktikum05/Sorting16.java
+++ Praktikum05/Sorting16.java
@@ -46,5 +46,17 @@ public class Sorting16 {
             data[min] = temp;
         }
     }
+
+    void insertionSort() {
+        for (int i = 1; i <= data.length-1; i++) {
+            int temp = data[i];
+            int j = i-1;
+            while (j >= 0 && data[j] > temp) {
+                data[j+1] = data[j];
+                j--;
+            }
+            data[j+1] = temp;
+        }
+    }
 }
 
